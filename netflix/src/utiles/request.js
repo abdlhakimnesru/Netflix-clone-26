680211fdc4b2e5541eb92a34168db756eb9178ff
Abdulhakim <abdlhakimnesru@gmail.com>
2026-05-08 16:48:03 +0300

    Banner

---
+++ netflix/src/utiles/request.js
@@ -0,0 +1,23 @@
+const API_KEY = "d2706343283bb25ed1b13987d065afe6";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
+  fetchTvShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+};
+
+export default requests;
